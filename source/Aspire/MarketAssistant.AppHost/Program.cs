ff076190d042a41d25a7a5f949983fde6dc292a6
Hamed Ansarian <hamed.ansarian@hotmail.com>
2025-03-12 13:38:18 +0100

    2

--- source/Aspire/MarketAssistant.AppHost/Program.cs
+++ source/Aspire/MarketAssistant.AppHost/Program.cs
@@ -21,69 +21,39 @@ var redis = builder.AddRedis("cache").WithDataVolume();
 var rabbit = builder.AddRabbitMQ("messaging")
     .WithDataVolume().WithManagementPlugin();
 
-// var postgresdb = builder.AddPostgres("postgresdb")
-//                         .WithHealthCheck(key: "postgresdb_healthcheck")
-//                         .WithPgAdmin()
-//                         .WithDataVolume()
-//                         .AddDatabase("marketassitant_db", "marketassitant_db");
-
 var sql = builder.AddSqlServer("sql", password: sqlPassword, 1433).WithDataVolume().WithImageTag("latest");
 
 var informingDb = sql.AddDatabase(name: "informingdb", databaseName: "informing");
 var identityDb = sql.AddDatabase(name: "identitydb", databaseName: "identity");
 var blockProcessorDb = sql.AddDatabase(name: "blockprocessordb", databaseName: "blockProcessor");
 
-//var informing = builder.AddProject<Projects.Informing_Grpc>("informing")
-//    // .WithEndpoint(endpointName: "http", options => 
-//    // {
-//    //     options.Protocol = System.Net.Sockets.ProtocolType.Tcp;
-//    //     options.Transport = HttpProtocol.Http3;
-//    //     //options.Port = 80;
-//    //     options.TargetPort = 80;
-//    // }, createIfNotExists: true)
-//    // .WithEndpoint(endpointName: "grpc", options => 
-//    // {
-//    //     options.Protocol = System.Net.Sockets.ProtocolType.Tcp;
-//    //     options.Transport = HttpProtocol.Http2;
-//    //     //options.Port = 81;
-//    //     options.TargetPort = 81;
-//    // }, createIfNotExists: true)
-//    .WithReference(redis)
-//    .WithReference(rabbit)
-//    .WithReference(informingDb)
-//    .WithEnvironment(name: UseInMemoryDatabase, value: builder.Configuration.GetValue(UseInMemoryDatabase, "true"))
-//    .WithEnvironment(name: EnsureDeletedDatabaseOnStartup, value: builder.Configuration.GetValue(EnsureDeletedDatabaseOnStartup, "false"))
-//    .WithEnvironment(name: "APPLICATION_NAME", value: "Informing")
-//    .WithEnvironment(name: TokenIssuer, value: builder.Configuration.GetValue(TokenIssuer, "https://identity.contoso.com"))
-//    .WithEnvironment(name: IdentitySecret, identitySecret)
-//    .WithEnvironment(name: "DISCORD_BOT_TOKEN", discordBotToken)
-//    .WaitFor(redis)
-//    .WaitFor(rabbit)
-//    .WaitFor(informingDb);
+var informing = builder.AddProject<Projects.Informing_Grpc>("informing")
+   .WithReference(redis)
+   .WithReference(rabbit)
+   .WithReference(informingDb)
+   .WithEnvironment(name: UseInMemoryDatabase, value: builder.Configuration.GetValue(UseInMemoryDatabase, "true"))
+   .WithEnvironment(name: EnsureDeletedDatabaseOnStartup, value: builder.Configuration.GetValue(EnsureDeletedDatabaseOnStartup, "false"))
+   .WithEnvironment(name: "APPLICATION_NAME", value: "Informing")
+   .WithEnvironment(name: TokenIssuer, value: builder.Configuration.GetValue(TokenIssuer, "https://identity.contoso.com"))
+   .WithEnvironment(name: IdentitySecret, identitySecret)
+   .WithEnvironment(name: "DISCORD_BOT_TOKEN", discordBotToken)
+   .WaitFor(redis)
+   .WaitFor(rabbit)
+   .WaitFor(informingDb);
 
-//var identity = builder.AddProject<Projects.BlockChainIdentity_Grpc>("identity")
-//    // .WithEndpoint(endpointName: "http", options => 
-//    // {
-//    //     options.Protocol = System.Net.Sockets.ProtocolType.Tcp;
-//    //     options.Transport = HttpProtocol.Http3;
-//    // }, createIfNotExists: true)
-//    // .WithEndpoint(endpointName: "grpc", options => 
-//    // {
-//    //     options.Protocol = System.Net.Sockets.ProtocolType.Tcp;
-//    //     options.Transport = HttpProtocol.Http2;
-//    // }, createIfNotExists: true)
-//    .WithReference(redis)
-//    .WithReference(rabbit)
-//    .WithReference(identityDb)
-//    .WithEnvironment(name: UseInMemoryDatabase, value: builder.Configuration.GetValue(UseInMemoryDatabase, "true"))
-//    .WithEnvironment(name: EnsureDeletedDatabaseOnStartup, value: builder.Configuration.GetValue(EnsureDeletedDatabaseOnStartup, "false"))
-//    .WithEnvironment(name: "APPLICATION_NAME", value: "Identity")
-//    .WithEnvironment(name: TokenIssuer, value: builder.Configuration.GetValue(TokenIssuer, "https://identity.contoso.com"))
-//    .WithEnvironment(name: IdentitySecret, identitySecret)
-//    .WithEnvironment(name: DatabaseEncryptionKey, value: builder.Configuration.GetValue<string>(DatabaseEncryptionKey))
-//    .WaitFor(redis)
-//    .WaitFor(rabbit)
-//    .WaitFor(identityDb);
+var identity = builder.AddProject<Projects.BlockChainIdentity_Grpc>("identity")
+   .WithReference(redis)
+   .WithReference(rabbit)
+   .WithReference(identityDb)
+   .WithEnvironment(name: UseInMemoryDatabase, value: builder.Configuration.GetValue(UseInMemoryDatabase, "true"))
+   .WithEnvironment(name: EnsureDeletedDatabaseOnStartup, value: builder.Configuration.GetValue(EnsureDeletedDatabaseOnStartup, "false"))
+   .WithEnvironment(name: "APPLICATION_NAME", value: "Identity")
+   .WithEnvironment(name: TokenIssuer, value: builder.Configuration.GetValue(TokenIssuer, "https://identity.contoso.com"))
+   .WithEnvironment(name: IdentitySecret, identitySecret)
+   .WithEnvironment(name: DatabaseEncryptionKey, value: builder.Configuration.GetValue<string>(DatabaseEncryptionKey))
+   .WaitFor(redis)
+   .WaitFor(rabbit)
+   .WaitFor(identityDb);
 
 var blockProcessorMigration = builder.AddProject<Projects.BlockProcessor_MigrationWorker>("blockprocessor-migrations")
     .WithReference(blockProcessorDb)
@@ -106,6 +76,8 @@ var blockProcessor = builder.AddProject<Projects.BlockProcessor_Api>("blockproce
     .WaitFor(rabbit)
     .WaitFor(blockProcessorDb)
     .WaitFor(blockProcessorMigration)
+    .WaitFor(identity)
+    .WaitFor(informing)
     .WithReplicas(2);
 
 //builder.AddProject<Projects.WalletTracker_Api>("wallettracker")
@@ -114,13 +86,16 @@ var blockProcessor = builder.AddProject<Projects.BlockProcessor_Api>("blockproce
 //    .WaitFor(redis)
 //    .WaitFor(rabbit);
 
-//builder.AddProject<Projects.ReverseProxy_Gateway>("reverseproxy-gateway")
-//    .WithReference(redis)
-//    .WithReference(identity)
-//    .WithReference(informing)
-//    .WithEnvironment(name: "APPLICATION_NAME", value: "ReverseProxy.Gateway")
-//    .WaitFor(redis)
-//    .WaitFor(rabbit)
-//    .WaitFor(identityDb);
+builder.AddProject<Projects.ReverseProxy_Gateway>("reverseproxy-gateway")
+   .WithReference(redis)
+   .WithReference(identity)
+   .WithReference(informing)
+   .WithReference(blockProcessor)
+   .WithEnvironment(name: "APPLICATION_NAME", value: "ReverseProxy.Gateway")
+   .WaitFor(redis)
+   .WaitFor(rabbit)
+   .WaitFor(identityDb)
+   .WaitFor(informing)
+   .WaitFor(blockProcessor);
 
 await builder.Build().RunAsync();
